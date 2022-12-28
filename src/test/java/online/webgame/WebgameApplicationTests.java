afcee1458a7f7f869529cbc6ab83fd8ff4858d68
Suvrat Agnihotri <suvrat.agnihotri@gingerwebs.co.in>
2022-12-28 21:29:27 +0530

    Initial commit

---
+++ src/test/java/online/webgame/WebgameApplicationTests.java
@@ -0,0 +1,13 @@
+package online.webgame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebgameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
